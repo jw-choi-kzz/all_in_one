9f18990a903239a4fa9b366c5854ad3d4c6e45a6
jw-choi-kzz <141205653+jw-choi-kzz@users.noreply.github.com>
2024-01-09 21:56:17 +0900

    Create  t1.java

---
+++ baekjoon/정렬/p11650_좌표정렬하기/t1.java
@@ -0,0 +1,37 @@
+package p11650_좌표정렬하기;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		int[][] num = new int[n][2];
+		
+		StringTokenizer st;
+		
+		for (int i = 0; i < n; i++) {
+			st = new StringTokenizer(br.readLine());
+			num[i][0] = Integer.parseInt(st.nextToken());
+			num[i][1] = Integer.parseInt(st.nextToken());
+		}
+		
+		
+		Arrays.sort(num,(o1,o2)->{ //배열, (이거,저거 있을 때) -> { if 이러면 뭐 return 기준 설정}
+			if (o1[0] == o2[0]) {
+				return o1[1] - o2[1];
+			} else {
+				return o1[0] - o2[0];
+			}
+		});
+		
+		for (int i = 0; i < n; i++) {
+			System.out.println(num[i][0]+" "+num[i][1]);
+		}
+		
+	} //main
+} //class
